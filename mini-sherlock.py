f52668312a750d48978db93861da1cc6008b3752
root <root@andrey-N616PC-M5F>
2020-07-30 22:04:44 +0300

    3.0

--- mini-sherlock.py
+++ mini-sherlock.py
@@ -59,7 +59,8 @@ imgsrcru = requests.head("https://imgsrc.ru/main/user.php?user="+nickname)
 dailymotion = requests.get("https://www.dailymotion.com/"+nickname)
 etsy = requests.get("https://www.etsy.com/shop/"+nickname)
 behance = requests.get("https://www.behance.net/"+nickname)
-telegram = requests.get("https://t.me/"+nickname)
+goodreads = requests.get("https://www.goodreads.com/"+nickname)
+instructables = requests.get("https://www.instructables.com/member/"+nickname)
 
 # Обработка
 
@@ -68,163 +69,170 @@ if  github.status_code == 200:
 else:
     print ("Github - не найденo")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  gitlab.status_code == 200:
     print ("Gitlab - https://gitlab.com/"+nickname)
 else:
     print ("Gitlab - не найденo")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  facebook.status_code == 200:
     print ("Facebook - https://www.facebook.com/"+nickname)
 else:
     print ("Facebook - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  instagram.status_code == 200:
     print ("Instagram - https://www.instagram.com/"+nickname)
 else:
     print ("Instagram - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  medium.status_code == 200:
     print ("Medium - https://medium.com/@"+nickname)
 else:
     print ("Medium - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  youtube.status_code == 200:
     print ("Youtube - https://youtube.com/c/"+nickname)
 else:
     print ("Youtube - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  soundcloud.status_code == 200:
     print ("Soundcloud - https://soundcloud.com/"+nickname)
 else:
     print ("Soundcloud - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  disqus.status_code == 200:
     print ("Disqus - https://disqus.com/"+nickname)
 else:
     print ("Disqus - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  pinterest.status_code == 200:
     print ("Pinterest - https://www.pinterest.com/"+nickname)
 else:
     print ("Pinterest - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  vimeo.status_code == 200:
     print ("Vimeo - https://vimeo.com/"+nickname)
 else:
     print ("Vimeo - не найден")
 
-os.system ("sleep 0.2")
+os.system ("sleep 0.1")
 
 if  aboutme.status_code == 200:
     print ("About.me - https://about.me/"+nickname)
 else:
     print ("About.me - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  flipboard.status_code == 200:
     print ("FlipBoard - https://flipboard.com/@"+nickname)
 else:
     print ("FlipBoard - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  slideshare.status_code == 200:
     print ("SlideShare - https://slideshare.net/"+nickname)
 else:
     print ("SlideShare - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  spotify.status_code == 200:
     print ("Spotify - https://open.spotify.com/user/"+nickname)
 else:
     print ("Spotify - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  scribd.status_code == 200:
     print ("Scribd - https://www.scribd.com/"+nickname)
 else:
     print ("Scribd - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  patreon.status_code == 200:
     print ("Patreon - https://www.patreon.com/"+nickname)
 else:
     print ("Patreon - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  bitbucket.status_code == 200:
     print ("BitBucket - https://bitbucket.org/"+nickname)
 else:
     print ("BitBucket - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  roblox.status_code == 200:
     print ("Roblox - https://www.roblox.com/user.aspx?username="+nickname)
 else:
     print ("Roblox - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  gravatar.status_code == 200:
     print ("Gravatar - http://en.gravatar.com/"+nickname)
 else:
     print ("Gravatar - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  imgsrcru.status_code == 200:
     print ("iMGSRC.RU - https://imgsrc.ru/main/user.php?user="+nickname)
 else:
     print ("iMGSRC.RU - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  dailymotion.status_code == 200:
     print ("DailyMotion - https://www.dailymotion.com/"+nickname)
 else:
     print ("DailyMotion - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  etsy.status_code == 200:
     print ("Etsy - https://www.etsy.com/shop/"+nickname)
 else:
     print ("Etsy - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
 if  behance.status_code == 200:
     print ("Behance - https://www.behance.net/"+nickname)
 else:
     print ("Behance - не найден")
 
-os.system("sleep 0.2")
+os.system("sleep 0.1")
 
-if  telegram.status_code == 200:
-    print ("Telegram - https://t.me/"+nickname)
+if  goodreads.status_code == 200:
+    print ("Goodreads - https://www.goodreads.com/"+nickname)
 else:
-    print ("Telegram - не найден")
+    print ("Goodreads - не найден")
+
+os.system("sleep 0.1")
+
+if  instructables.status_code == 200:
+    print ("Instructables - https://www.instructables.com/member/"+nickname)
+else:
+    print ("Instructbales - не найден")
